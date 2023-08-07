774dab85a95f56ac5cdac3e51257260fa1022cd7
shlomo masuri <fcshlom@gmail.com>
2023-08-07 11:22:00 +0300

    add new test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,6 +3,7 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './core/components/nav-bar/nav-bar.component';
 import { MatToolbar } from '@angular/material/toolbar';
+import { async } from 'rxjs';
 
 describe('AppComponent', () => {
   beforeEach(() =>
@@ -14,7 +15,7 @@ describe('AppComponent', () => {
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
@@ -22,5 +23,18 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('lumix-search-task');
+  });
+
+  it('should display nav bar', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const app = fixture.componentInstance;
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('app-nav-bar')).toBeTruthy;
+    expect(compiled.querySelector('router-outlet')).toBeTruthy;
+    expect(compiled.querySelector('app-nav-bar mat-toolbar h3')).toBeTruthy;
+    expect(
+      compiled.querySelector('app-nav-bar mat-toolbar h3').textContent
+    ).toContain('Lumix Search task');
   });
 });
